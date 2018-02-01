b0200352060e50200e87cfc54badfeb65b2ac19c
yoshiki_inaya <yoshiki_inaya@oss-g.omron.co.jp>
2018-02-01 17:33:19 +0900

    refine multiprocessing

--- ToxicComment.py
+++ ToxicComment.py
@@ -11,27 +11,27 @@ warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 
 
 def wakatiWithTT(txt):
-    
     tagdir = os.getenv('TREETAGGER_ROOT')
     tagger = treetaggerwrapper.TreeTagger(TAGLANG='en', TAGDIR=tagdir)
     tags = tagger.TagText(txt)
     validPos = ['VV', 'NN', 'NP', 'JJ', 'VH']
     extractedTag = list()
+    [extractedTag.append(tag.split()[2]) for tag in tags if tag.split()[1][0:2] in validPos]
+    '''
     for tag in tags:
         sptag = tag.split()
-        if (sptag[1][0: 2] in validPos):
+        if (sptag[1][0:2] in validPos):
             extractedTag.append(sptag[2])
+    '''
     return extractedTag
 
 
-def process(i):
-    return i
-
 if __name__ == '__main__':
-    # train Data
+    # get train Data
     data = pd.read_csv('./data/train.csv', encoding='utf-8')
     attrs = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-    
+
+    # Preprocessing train Data
     with Pool(multi.cpu_count()) as p:
         wakati = list(tqdm.tqdm(p.imap(wakatiWithTT, data['comment_text']), total=data['comment_text'].size))
     data['CommentTxtWakati'] = pd.Series(wakati)
